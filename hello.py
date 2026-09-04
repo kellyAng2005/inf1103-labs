f642ca5cbaca0a8aca051ee0dea7d41f7885e54a
kellyAng <2605691@sit.singaporetech.edu.sg>
2026-09-04 10:41:50 +0800

    second activity

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("==============================")
 print("Welcome here")
 print("My first post!")
 print("==============================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("username:", username)
+print("bio:", bio)
+print("followers:", followers)
